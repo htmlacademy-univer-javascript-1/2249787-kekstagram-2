368aecd788732dd7548edb3fd53bebd9af69da0c
Reina <zxe6@yandex.ru>
2023-03-05 20:01:42 +0500

    fixed: добавлена операция, исправлены чеки

--- js/main.js
+++ js/main.js
@@ -1,24 +1,17 @@
-function getRandomNumber(min, max) { 
-    if (min < max && min >= 0 && max >= 0) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    } 
-    if (min >= max) {
-        return 'Incorrect value entry: minimum is greater than maximum';
-    }
-    if (max < 0 || min < 0) {
-        throw new RangeError('Minimum and maximum values must be positive');
-    }
+function getRandomNumber(min, max) {
+  if (min < max && min >= 0 && max >= 0) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
   }
-  
-
-const checkMaxSymbol = (str, maxLength) => {
-    return str.length <= maxLength;
+  if (min >= max) {
+    [min, max] = [max, min];
+    min = Math.ceil(min);
+    max = Math.floor(max);
+  }
+  if (max < 0 || min < 0) {
+    throw new RangeError('Minimum and maximum values must be positive');
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-/* Источники: лекции в Teams,
-https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length ,
-https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random ,
-https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-*/
+const checkMaxSymbol = (str, maxLength) => str.length <= maxLength;
