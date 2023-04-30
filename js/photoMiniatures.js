88cdcd8ea77d066c5415562bc0e60d52f0d0534a
Reina <zxe6@yandex.ru>
2023-04-30 18:59:51 +0500

    Отрисуй меня полностью (часть 2)

--- js/photoMiniatures.js
+++ js/photoMiniatures.js
@@ -1,3 +1,5 @@
+import {showPhotoFull} from './photoFull.js'
+
 const pictureTemplate = document.querySelector('#picture');
 const pictureContainer = document.querySelector('.pictures');
 
@@ -6,9 +8,14 @@ export function showPhotoMini(descriptions) {
   for (const desc of descriptions) {
     const picture = pictureTemplate.cloneNode(true).content;
     picture.querySelector('.picture__img').src = desc.url;
+    picture.querySelector('.picture__img').addEventListener('click', (ev) => {
+      ev.preventDefault();
+      showPhotoFull(desc);
+    });
+
     picture.querySelector('.picture__likes').textContent = desc.likes;
     picture.querySelector('.picture__comments').textContent = desc.comments.length;
     pictureFragment.appendChild(picture);
   }
-  pictureFragment.appendChild(pictureFragment);
+  pictureContainer.appendChild(pictureFragment);
 } 
